69eb28b519edfc8f373a4aadf2157e5d449ef980
wintig <840872473@qq.com>
2019-12-15 19:44:24 +0800

    rabbitmq 整合spring boot

---
+++ rabbitmq/rabbit-mq-RPC模式/src/main/java/com/wintig/rabbitmqRPC/RabbitMqRpc模式Application.java
@@ -0,0 +1,13 @@
+package com.wintig.rabbitmqRPC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitMqRpc模式Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitMqRpc模式Application.class, args);
+	}
+
+}
---
+++ rabbitmq/rabbit-mq-RPC模式/src/test/java/com/wintig/rabbitmqRPC/RabbitMqRpc模式ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wintig.rabbitmqRPC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitMqRpc模式ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
